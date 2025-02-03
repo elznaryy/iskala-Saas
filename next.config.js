0a609dba1ff25cba971e950627179bf07e6aeaec
elznary <elznary213@gmail.com>
2025-02-03 18:18:35 +0200

    test3

--- next.config.js
+++ next.config.js
@@ -13,6 +13,19 @@ const nextConfig = {
       },
     ]
   },
+  // Add these options to ignore TypeScript errors in production
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  // Optionally, also ignore ESLint errors
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
